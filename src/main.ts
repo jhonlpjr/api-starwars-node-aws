3d47a9609157d50785055c92eb8a682cd00d32d1
Jonathan Reyna <jonathan.reyna@onroadts.com>
2022-10-21 00:50:43 -0500

    Cleaning up unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { Callback, Context, Handler } from 'aws-lambda';
-import serverlessExpress from '@vendia/serverless-express';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { useContainer } from '@nestjs/class-validator';
 import { Express } from 'express';
